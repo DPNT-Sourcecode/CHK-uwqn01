451b5dd825b2af49d031b2b777d4b2aefc989d14
sbx_user1051 <sbx_user1051@169.254.27.89>
2024-08-07 20:47:30 +0000

    Wed Aug 07 20:47:30 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,7 +4,7 @@ special_offers = {"A":[3,130],"B":[2,45]}
 
 def checkout(skus: str) -> int:
     # if the items entered is not A,B,C,D then also want to return -1
-    list_of_items: list = skus.split(",")
+    list_of_items: list = skus.split(", ")
     checkout_dict = {}
     special_offers_price = 0
     total = 0
@@ -25,4 +25,5 @@ def checkout(skus: str) -> int:
             checkout_dict[grocery_item] = (count_grocery_item % special_offers[grocery_item][0])
         total += checkout_dict[grocery_item]*price_list[grocery_item]
     return total
+
 
