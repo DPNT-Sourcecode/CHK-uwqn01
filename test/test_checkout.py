b24b34291c459f3d8004482316cc710abeab5f55
sbx_user1051 <sbx_user1051@169.254.27.89>
2024-08-07 20:56:30 +0000

    Wed Aug 07 20:56:30 UTC 2024

--- test/test_checkout.py
+++ test/test_checkout.py
@@ -17,3 +17,15 @@ def test_checkout_invalid():
 def test_special_offer_checkout():
     basket = checkout("A, A, A, B, C, D")
     assert basket == 195
+
+def test_special_offer_checkout2():
+    basket = checkout("A, A, C, B, B, D")
+    assert basket == 180
+
+def test_special_offer_checkout3():
+    basket = checkout("A, A, A, A")
+    assert basket == 180
+
+def test_special_offer_checkout4():
+    basket = checkout("A, A, A, B, B, B")
+    assert basket == 205
