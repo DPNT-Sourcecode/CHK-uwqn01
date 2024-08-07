64db829a83ea75f37e8a16a706d58ca8919d9081
sbx_user1051 <sbx_user1051@169.254.27.89>
2024-08-07 20:14:30 +0000

    Wed Aug 07 20:14:30 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,5 +2,15 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
-def checkout(skus):
-    raise NotImplementedError()
+def checkout(skus: str) -> int:
+    # if the items entered is not A,B,C,D then also want to return -1
+    list_of_items: list = skus.split()
+    checkout_dict = {}
+    for item in list_of_items:
+        if item not in checkout_dict:
+            checkout_dict[item] = 1
+        else:
+            checkout_dict[item] += 1
+
+    
+
