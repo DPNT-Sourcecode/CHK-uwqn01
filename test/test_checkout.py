7f26929dea894cc5e62dea98c3b95fbaa2befe06
sbx_user1051 <sbx_user1051@169.254.27.89>
2024-08-07 20:53:30 +0000

    Wed Aug 07 20:53:30 UTC 2024

--- test/test_checkout.py
+++ test/test_checkout.py
@@ -3,9 +3,17 @@ from solutions.CHK.checkout_solution import checkout
 
 
 def test_checkout():
-    test1 = checkout("A, B, C, D")
-    assert test1 == 115
+    basket = checkout("A, B, C, D")
+    assert basket == 115
 
 def test_checkout_empty():
-    test1 = checkout("")
-    assert test1 == -1
+    basket = checkout("")
+    assert basket == -1
+
+def test_checkout_invalid():
+    basket = checkout("A, B, C, E")
+    assert basket == -1
+
+def test_special_offer_checkout():
+    basket = checkout("A, A, A, B, C, D")
+    assert basket == 195
