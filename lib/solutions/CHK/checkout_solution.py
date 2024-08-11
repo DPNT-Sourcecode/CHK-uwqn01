c74a9468d310f9cec46105337367f4b7c5d1dcc8
sbx_user1051 <sbx_user1051@169.254.85.13>
2024-08-11 11:59:57 +0000

    Sun Aug 11 11:59:57 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -50,13 +50,13 @@ special_offers = {
     ]
 }
 
-
+group_offers = []
 class GroupOffers():
     def __init__(self, grp_items: list, qty_req: int, price: int):
         self.grp_items = grp_items
         self.qty_req = qty_req
         self.price = price
-        group_offers = grp_items
+
 
 def checkout(skus: str) -> int:
     if skus == "":
@@ -96,5 +96,6 @@ def checkout(skus: str) -> int:
                 checkout_dict[grocery_item] = (count_grocery_item % deal.qty)
         total += checkout_dict[grocery_item]*price_list[grocery_item]
     return total
+
 
 
